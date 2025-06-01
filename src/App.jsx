a9ef3b369231093fb2cbc6664da2ae0d086f0e1c
seif <mohamedahmedseifofficial@gmail.com>
2025-06-02 00:49:33 +0300

    creating AppLayout as parent router, using Outlet to render dynamicaly

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,43 @@ import Menu from './features/menu/Menu'
 import Order from './features/order/Order'
 import CreateOrder from './features/order/CreateOrder'
 import Cart from './features/cart/Cart'
+import AppLayout from "./ui/AppLayout"
 
 
 function App() {
   /*This syntax is new in react-router v.6, and it's necessary to enable data loading*/
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/menu",
+          element: <Menu />
+        },
+        {
+          path: "/cart",
+          element: <Cart />
+        },
+        {
+          path: "/order",
+          element: <Order />
+        },
+        {
+          path: "/order/new",
+          element: <CreateOrder />
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />
+        },
+      ]
     },
-    {
-      path: "/menu",
-      element: <Menu />
-    },
-    {
-      path: "/cart",
-      element: <Cart />
-    },
-    {
-      path: "/order",
-      element: <Order />
-    },
-    {
-      path: "/order/new",
-      element: <CreateOrder />
-    },
-    {
-      path: "/order/:orderId",
-      element: <Order />
-    },
-
   ])
+  
   return (
     <RouterProvider router={router}></RouterProvider>
   )
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router"
 import Header from "./Header"
 
 function AppLayout() {
   return (
     <div>
         <Header></Header>
+        <main>
+            <Outlet></Outlet>
+        </main>
+        <footer><h2>this is footer</h2></footer>
     </div>
   )
 }
